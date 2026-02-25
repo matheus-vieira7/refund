041a46025b7359712c3261fdd5b1f3a8026738be
matheus santos <matheusvieira157k@gmail.com>
2026-02-25 18:40:26 -0300

    finalizando

---
+++ scripts.js
@@ -0,0 +1,85 @@
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+
+const expenseList = document.querySelector("ul");
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+  value = Number(value) / 100;
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  return value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  expenseadd(newExpense);
+};
+
+function expenseadd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("Expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    expenseInfo.append(expenseName, expenseCategory)
+    const expenseAmount = document.createElement("span");
+
+  expenseAmount.classList.add("expense-amount");
+  expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+  .toUpperCase()
+  .replace("R$", "")}`;
+
+    const removeIcon =  document.createElement("img")
+    removeIcon.classList.add("remove-Icon")
+    removeIcon.setAttribute("src","img/remove.svg")
+    removeIcon.setAttribute("alt", "remove")
+
+expenseItem.append(expenseIcon, expenseInfo, expenseAmount,removeIcon);
+
+
+    
+    const expenseText = document.createElement("span");
+    expenseText.textContent = `${newExpense.expense} - ${newExpense.amount}`;
+
+    expenseItem.appendChild(expenseText);
+    expenseList.appendChild(expenseItem);
+    
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas");
+    console.log(error);
+  }
+}
+
+  
+
